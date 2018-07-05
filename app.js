3fa64c67bbf2191fef063dd6fc0d4b88a8824681
See Loo Jane <muigek34@yahoo.com.sg>
2018-07-05 15:56:15 +0800

    add swagger documentations + update relevant files to serve it

--- app.js
+++ app.js
@@ -1,7 +1,10 @@
 const express = require("express");
 const app = express();
+const swaggerUi = require('swagger-ui-express');
+const swaggerDocument = require('./swagger-sample.json');
 const indexRouter = require("./routes/indexRouter");
 const carparkRouter = require("./routes/carparkRouter");
+
 
 const carparkErrorHandler = (req, res, next) => {
 	if (res.statusCode === 404) {
@@ -11,6 +14,7 @@ const carparkErrorHandler = (req, res, next) => {
 };
 
 app.use(express.json());
+app.use("/api-docs", swaggerUi.serve, swaggerUi.setup(swaggerDocument));
 app.use("/", indexRouter);
 app.use("/carparks", carparkRouter, carparkErrorHandler);
 module.exports = app;
--- utils/globals.js
+++ utils/globals.js
@@ -2,7 +2,7 @@ const carparkTypes = {
 	basement: "BASEMENT CAR PARK",
 	multistorey: "MULTI-STOREY CAR PARK",
 	surface: "SURFACE CAR PARK",
-	mechanism: "MECHANISED CAR PARK",
+	mechanised: "MECHANISED CAR PARK",
 	covered: "COVERED CAR PARK",
 	mechanised_and_surface: "MECHANISED AND SURFACE CAR PARK",
 	surface_and_multistorey: "SURFACE/MULTI-STOREY CAR PARK"
